a17c87a80538fd7882a0ba74dfcce1268a3e5f52
Julius Plocios <julius.plocios@yahoo.com>
2018-05-26 15:29:46 +0800

    creating models

--- backend/db/models.js
+++ backend/db/models.js
@@ -5,8 +5,32 @@ var userSchema = new Schema({
   name: String,
 });
 
+var accountSchema = new Schema({
+
+});
+
+var transactionSchema = new Schema({
+
+});
+
+var procurementItemSchema = new Schema({
+
+});
+
+var pickUpSchema = new Schema({
+
+});
+
 var User = mongoose.model('User', userSchema);
+var Account = mongoose.model('Account', accountSchema);
+var Transaction = mongoose.model('Transaction', transactionSchema);
+var ProcurementItem = mongoose.model('ProcurementItem', procurementItemSchema);
+var PickUp = mongoose.model('PickUp', pickUpSchema);
 
 module.exports = {
-  User
+  User,
+  Account,
+  Transaction,
+  ProcurementItem,
+  PickUp
 };
