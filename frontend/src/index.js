a6c87d9e6cc77bbb89fdc6841cb956449d0ed67e
Daniel Francia <daniels7edge@gmail.com>
2018-05-26 13:28:41 +0800

    redux,react-redux, thunk and promise

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux'
+import reducer from './store/reducer'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(thunk),)
+    )
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
