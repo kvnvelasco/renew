e4abda51c8a5e2ad969e5aa28e0ccb231a5b196f
Julius Plocios <julius.plocios@yahoo.com>
2018-05-26 14:37:43 +0800

    setup mongo connetion

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const mongoose = require('./db/mongodb-connect');
+
+var { User } = require('./db/models');
+
+
+app.listen(port, () => console.log(`Server is up on port ${port}`));
