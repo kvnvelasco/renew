7af984d15a94a776553fcb68d767eac8a2d83c24
Kevin Velasco <me@kvnvelasco.com>
2018-05-26 15:28:23 +0800

    add api controls

---
+++ backend/src/api/user.js
@@ -0,0 +1,10 @@
+
+/**
+ * User controller
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {*} next 
+ */
+function userController(req, res, next) {
+  // get the current user with the model somehow
+}
