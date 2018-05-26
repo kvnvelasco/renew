1e96d5bc7097ef7389803607efc6613c7801c7f9
Julius Plocios <julius.plocios@yahoo.com>
2018-05-26 15:55:51 +0800

    wip

--- backend/db/models.js
+++ backend/db/models.js
@@ -2,30 +2,39 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  name: String,
-});
-
-var accountSchema = new Schema({
-
+  name: String
 });
 
 var transactionSchema = new Schema({
-
+  date: {
+    type: Date, default: Date.now 
+  },
+  product_name: String,
+  user: String
 });
 
-var procurementItemSchema = new Schema({
-
+var productSchema = new Schema({
+  product_name: String,
+  details: String
 });
 
 var pickUpSchema = new Schema({
-
+  pickup_date: Date,
+  address: String
 });
 
+var deliverySchema = new Schema({
+  delivery_date: Date,
+  product_name: String,
+  recipient: String,
+  address: String
+})
+
 var User = mongoose.model('User', userSchema);
-var Account = mongoose.model('Account', accountSchema);
 var Transaction = mongoose.model('Transaction', transactionSchema);
-var ProcurementItem = mongoose.model('ProcurementItem', procurementItemSchema);
+var Product = mongoose.model('Product', productSchema);
 var PickUp = mongoose.model('PickUp', pickUpSchema);
+var Delivery = mongoose.model('Delivery', deliverySchema)
 
 module.exports = {
   User,
