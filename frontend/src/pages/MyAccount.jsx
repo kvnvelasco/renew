f0c464a15f08ef0e4f6e22916eba9e4476aaf151
tonytonyy1 <delacruztony.adc@gmail.com>
2018-05-26 14:02:27 +0800

    ROUTING

---
+++ frontend/src/pages/MyAccount.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MyAccount extends Component {
+    render(){
+        return (
+            <div>
+                <h1>MY ACCOUNT PAGE</h1>
+            </div>
+        )
+    }
+}
