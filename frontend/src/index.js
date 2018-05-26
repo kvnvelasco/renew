6e3d5a01669d28297641da3787f2805902441599
Kevin Velasco <me@kvnvelasco.com>
2018-05-27 07:25:51 +0800

    partial fix navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
-import { BrowserRouter, Route, Link } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux'
-import reducer from './store/reducer'
-import thunk from 'redux-thunk'
-import store from './store/store'
+import store from './store/store';
 
 ReactDOM.render(
     <Provider store={store}>
