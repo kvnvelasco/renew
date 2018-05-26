0bc9c372356a22adad0dcda3b76b83a186c8c24f
tonytonyy1 <delacruztony.adc@gmail.com>
2018-05-26 14:07:44 +0800

    index route fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,18 +9,18 @@ import { Provider } from 'react-redux'
 import reducer from './store/reducer'
 import thunk from 'redux-thunk'
 
-<<<<<<< HEAD
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, document.getElementById('root'));
-=======
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducer,
-    composeEnhancers(applyMiddleware(thunk),)
-    )
+    composeEnhancers(applyMiddleware(thunk), )
+)
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
->>>>>>> a6c87d9e6cc77bbb89fdc6841cb956449d0ed67e
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>, document.getElementById('root'));
+a6c87d9e6cc77bbb89fdc6841cb956449d0ed67e
 registerServiceWorker();
