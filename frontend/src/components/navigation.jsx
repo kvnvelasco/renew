39049bd0df882f9013143edbfd899e71a7e82f81
tonytonyy1 <delacruztony.adc@gmail.com>
2018-05-27 07:19:12 +0800

    took off acc in nav bar / changed vits to REN

--- frontend/src/components/navigation.jsx
+++ frontend/src/components/navigation.jsx
@@ -6,29 +6,30 @@ import { NavLink, Link } from 'react-router-dom';
 
 import * as React from 'react';
 
-export default function Navigation(props){
+export default function Navigation(props) {
     return (
         <nav className="nav-container">
             <ul className='nav--links'>
                 <li className='nav--link-item'>
-                    <img className="logo" alt="logo" src={logo}/>
+                    <img className="logo" alt="logo" src={logo} />
                 </li>
                 <li className='nav--link-item'>
                     <NavLink to="/procurement" className='button'>Procurement</NavLink>
                 </li>
                 <li className='nav--link-item'>
-            <NavLink to="/orders" className='button'>Pickups / Deliveries</NavLink>
+                    <NavLink to="/orders" className='button'>Pickups / Deliveries</NavLink>
                 </li>
-                <li className='nav--link-item'>
+                {/* <li className='nav--link-item'>
                     <NavLink to="/account" className='button'>My&nbsp;Account</NavLink>
-                </li>
+                </li> */}
                 <li className='nav--link-item'>
                     <span>Your Balance</span>
-                    <div className="balance"> Vits</div>
+                    <div className="balance"><strong>3400</strong> REN</div>
                 </li>
                 <li className='nav--link-item'>
-                    <img className="cart" alt="cart" src={cart}/>
+                    <img className="cart" alt="cart" src={cart} />
                 </li>
             </ul>
-        </nav>)
+        </nav>
+    )
 }
