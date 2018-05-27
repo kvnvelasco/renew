e97db926267884783cb05545feb8202fc395fc0a
Kevin Velasco <me@kvnvelasco.com>
2018-05-27 12:10:10 +0800

    add images correctly

--- backend/db/init.js
+++ backend/db/init.js
@@ -98,25 +98,25 @@ const categories = [
         name: "Plastic Bin",
         description: "Store classroom supplies",
         price: 120,
-        iamgeUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP5C3zCOafzq1uzMzwQSRUAfVPzerY5TpLX6BGNQ5p7ehrDTXw"
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP5C3zCOafzq1uzMzwQSRUAfVPzerY5TpLX6BGNQ5p7ehrDTXw"
       },
       {
         name: "Pencil Case",
         description: "Store classroom supplies",
         price: 40,
-        iamgeUrl: "https://i5.walmartimages.com/asr/565d6ab3-9a57-4bb5-ba82-3f6556d882d4_1.81ef282f50d90f673b1f81e2607f0d53.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF"
+        image_url: "https://i5.walmartimages.com/asr/565d6ab3-9a57-4bb5-ba82-3f6556d882d4_1.81ef282f50d90f673b1f81e2607f0d53.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF"
       },
       {
         name: "Serving Tray",
         description: "For cafeteria use",
         price: 40,
-        iamgeUrl: "https://image.made-in-china.com/2f0j00dZOQSvMzETbt/Skidproof-Plastic-Service-Tray.jpg"
+        image_url: "https://image.made-in-china.com/2f0j00dZOQSvMzETbt/Skidproof-Plastic-Service-Tray.jpg"
       },
       {
         name: "Clipboards",
         description: "Self explanatory",
         price: 60,
-        iamgeUrl: "https://sc01.alicdn.com/kf/HTB19k4qSFXXXXaSaFXXq6xXFXXXn/A4-Size-Transparent-Plastic-Clipboards-With-Low.jpg_350x350.jpg"
+        image_url: "https://sc01.alicdn.com/kf/HTB19k4qSFXXXXaSaFXXq6xXFXXXn/A4-Size-Transparent-Plastic-Clipboards-With-Low.jpg_350x350.jpg"
       },
     ]
   },
@@ -126,13 +126,13 @@ const categories = [
         name: "ABS 0.5mm 1.2KG",
         description: "Recycled ABS fillament",
         price: 2300,
-        iamgeUrl: "https://cdn.shopify.com/s/files/1/0778/8857/products/Blue-ABS-Clean-cropped_large.jpg?v=1442137414"
+        image_url: "https://cdn.shopify.com/s/files/1/0778/8857/products/Blue-ABS-Clean-cropped_large.jpg?v=1442137414"
       },
       {
         name: "Clear PET 0.5mm 1.2kg",
         description: "Recycled PET fillament",
         price: 1800,
-        iamgeUrl: "https://cdn.shopify.com/s/files/1/2104/4351/products/pet-0301.png?v=1501010720"
+        image_url: "https://cdn.shopify.com/s/files/1/2104/4351/products/pet-0301.png?v=1501010720"
       },
       {
         name: "Green PET 0.5mm 1.2kg",
@@ -144,7 +144,7 @@ const categories = [
         name: "Blue PET 0.5mm 1.2kg",
         description: "Recycled PET fillament",
         price: 1800,
-        iamgeUrl: "https://cdn.shopify.com/s/files/1/1869/6613/products/PETG_transparent.jpg?v=1493185533"
+        image_url: "https://cdn.shopify.com/s/files/1/1869/6613/products/PETG_transparent.jpg?v=1493185533"
       },
     ]
   },
@@ -154,19 +154,19 @@ const categories = [
         name: "PET clear, shredded",
         description: "Processed clear PET for manufacturing",
         type: 'auction',
-        iamgeUrl: "http://www.hiwtc.com/photo/products/11/00/01/105.jpg"
+        image_url: "http://www.hiwtc.com/photo/products/11/00/01/105.jpg"
       },
       {
         name: "PET green, shredded",
         description: "Processed green PET for manufacturing",
         type: 'auction',
-        iamgeUrl: "https://sc02.alicdn.com/kf/HTB1jtY0KVXXXXaPaXXXq6xXFXXXu/Shredded-PET-from-bottled-water-and-plastic.jpg_220x220.jpg"
+        image_url: "https://sc02.alicdn.com/kf/HTB1jtY0KVXXXXaPaXXXq6xXFXXXu/Shredded-PET-from-bottled-water-and-plastic.jpg_220x220.jpg"
       },
       {
         name: "ABS",
         description: "Recycled ABS from multiple sources",
         type: 'auction',
-        iamgeUrl: "http://www.recicladoplastico.com.ar/wp-content/uploads/2016/07/plastico-inyeccion.jpg"
+        image_url: "http://www.recicladoplastico.com.ar/wp-content/uploads/2016/07/plastico-inyeccion.jpg"
       },
     ]
   },
@@ -176,57 +176,57 @@ const categories = [
         name: "Nylon Vase",
         description: "Designer vase",
         type: 'single',
-        iamgeUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSURKlZvThRqftNPXokcV5IjEn7iMp0j_ysYfTtnDrHvQoz8L8F9Q"
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSURKlZvThRqftNPXokcV5IjEn7iMp0j_ysYfTtnDrHvQoz8L8F9Q"
       }, {
         name: "Multi-Color Lamp",
         description: "Designer Lamp",
         type: 'single',
-        iamgeUrl: "http://www.freshdesignpedia.com/wp-content/uploads/plastic-art-and-home-accessories-from-straws/plastic-art-straws-colorful-light.jpg"
+        image_url: "http://www.freshdesignpedia.com/wp-content/uploads/plastic-art-and-home-accessories-from-straws/plastic-art-straws-colorful-light.jpg"
       }, {
         name: "Plants with Vases",
         description: "Designer vase",
         type: 'single',
-        iamgeUrl: "https://inhabitat.com/wp-content/blogs.dir/1/files/2015/04/Plastic-bottle-art-by-Veronika-Richterova-9.jpg"
+        image_url: "https://inhabitat.com/wp-content/blogs.dir/1/files/2015/04/Plastic-bottle-art-by-Veronika-Richterova-9.jpg"
       }, {
         name: "Fish Sclupture",
         description: "Sculpture",
         type: 'single',
-        iamgeUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM0wEwjvRkL80nNxM1ioueqPfrNGj-jpansDMbiLI3EBthfsGYBQ"
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM0wEwjvRkL80nNxM1ioueqPfrNGj-jpansDMbiLI3EBthfsGYBQ"
       }, {
         name: "Roses",
         description: "Flower art",
         type: 'single',
-        iamgeUrl: "https://grist.files.wordpress.com/2014/09/ar-vortexas.jpg?w=660"
+        image_url: "https://grist.files.wordpress.com/2014/09/ar-vortexas.jpg?w=660"
       }, {
         name: "Miniatures",
         description: "Sculptures/Figures",
         type: 'single',
-        iamgeUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjaJwUqNq1XZ2P5o2zJyNk7TYUE0BfgBzJU4Jko6untLbXyOW-"
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjaJwUqNq1XZ2P5o2zJyNk7TYUE0BfgBzJU4Jko6untLbXyOW-"
       }, {
         name: "Flower Vase",
         description: "Designer vase",
         type: 'single',
-        iamgeUrl: "http://www.eatonquade.com/photos/full/fEQVASE.jpg"
+        image_url: "http://www.eatonquade.com/photos/full/fEQVASE.jpg"
       }, {
         name: "Wall Decor",
         description: "Designer Input",
         type: 'single',
-        iamgeUrl: "https://www.e-flux.com/wp-content/uploads/2010/11/wpid-1195923815image_web.jpg?b8c429,1440"
+        image_url: "https://www.e-flux.com/wp-content/uploads/2010/11/wpid-1195923815image_web.jpg?b8c429,1440"
       }, {
         name: "Pillar Sculptures",
         description: " Abstract Art",
         type: 'single',
-        iamgeUrl: "http://www.dianaboulay.com/x_trash-art_recycled_plastic_arts_sculpture_from_waste_found_objects_Serendipity_2_by_Diana_Boulay_from_Canada.jpg"
+        image_url: "http://www.dianaboulay.com/x_trash-art_recycled_plastic_arts_sculpture_from_waste_found_objects_Serendipity_2_by_Diana_Boulay_from_Canada.jpg"
       }, {
         name: "Flower Decor",
         description: "Hangable Decor",
         type: 'single',
-        iamgeUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOmS8dK9LWFkdPqNCLA8QeWT8ovxjFqU3TocNtKLAk-xQIaQyNZQ"
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOmS8dK9LWFkdPqNCLA8QeWT8ovxjFqU3TocNtKLAk-xQIaQyNZQ"
       }, {
         name: "Human Size Sculpture",
         description: "Human Scale plastic art",
         type: 'single',
-        iamgeUrl: "http://www.irazoqui.net/uploads/1/2/9/4/12949800/5207212.jpg?295"
+        image_url: "http://www.irazoqui.net/uploads/1/2/9/4/12949800/5207212.jpg?295"
       }
     ]
   }
